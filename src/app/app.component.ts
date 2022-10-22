af46bf203d7e162635a5b1c7921cde35da17687d
lucas arcanjo <lucas.saolliveira@gmail.com>
2022-10-21 22:33:51 -0300

    consolidating knowledge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-pictures';
+  photos = [
+    {
+      url: 'https://images.pexels.com/photos/13366951/pexels-photo-13366951.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      description: 'horizonte'
+    },
+    {
+      url: 'https://images.pexels.com/photos/2700567/pexels-photo-2700567.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      description: 'lago'
+    },
+    {
+      url: 'https://images.pexels.com/photos/13366951/pexels-photo-13366951.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      description: 'horizonte'
+    }
+  ]
 }
